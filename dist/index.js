55f64f848202c02b188d580ea3d64d4139711bd5
afeiship <1290657123@qq.com>
2020-11-20 22:38:37 +0800

    fix: update prpoxy code

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+/*!
+ * name: @jswork/next-fetch-with-proxy
+ * description: Fetch with proxy options.
+ * homepage: https://github.com/afeiship/next-fetch-with-proxy
+ * version: 1.0.0
+ * date: 2020-11-20 22:38:26
+ * license: MIT
+ */
+
+(function () {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('@jswork/next');
+  var ProxyAgent = require('proxy-agent');
+  var DEFAULT_OPTIONS = { proxy: null };
+
+  nx.fetchWithProxy = function (inFetch) {
+    return function (inUrl, inOptions) {
+      var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+      options.agent = options.proxy ? new ProxyAgent(options.proxy) : null;
+      return inFetch(inUrl, options);
+    };
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.fetchWithProxy;
+  }
+})();
